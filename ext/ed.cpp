28af03d1512d8371f1d1e09f76d70081a09ca9f1
Chris Hall <chall@pangeare.com>
2014-12-09 19:57:09 -0600

    Imported ssl_version setting from EventMachine-LE
    This adds an :ssl_version option to the start_tls method that allows the
    user to specify the version of SSL to use.

--- ext/ed.cpp
+++ ext/ed.cpp
@@ -381,6 +381,7 @@ ConnectionDescriptor::ConnectionDescriptor (int sd, EventMachine_t *em):
 	SslBox (NULL),
 	bHandshakeSignaled (false),
 	bSslVerifyPeer (false),
+	bSslVersion (0),
 	bSslPeerAccepted(false),
 	#endif
 	#ifdef HAVE_KQUEUE
@@ -1133,7 +1134,7 @@ void ConnectionDescriptor::StartTls()
 	if (SslBox)
 		throw std::runtime_error ("SSL/TLS already running on connection");
 
-	SslBox = new SslBox_t (bIsServer, PrivateKeyFilename, CertChainFilename, bSslVerifyPeer, GetBinding());
+	SslBox = new SslBox_t (bIsServer, PrivateKeyFilename, CertChainFilename, bSslVerifyPeer, bSslVersion, GetBinding());
 	_DispatchCiphertext();
 	#endif
 
@@ -1147,7 +1148,7 @@ void ConnectionDescriptor::StartTls()
 ConnectionDescriptor::SetTlsParms
 *********************************/
 
-void ConnectionDescriptor::SetTlsParms (const char *privkey_filename, const char *certchain_filename, bool verify_peer)
+void ConnectionDescriptor::SetTlsParms (const char *privkey_filename, const char *certchain_filename, bool verify_peer, int ssl_version)
 {
 	#ifdef WITH_SSL
 	if (SslBox)
@@ -1157,6 +1158,7 @@ void ConnectionDescriptor::SetTlsParms (const char *privkey_filename, const char
 	if (certchain_filename && *certchain_filename)
 		CertChainFilename = certchain_filename;
 	bSslVerifyPeer = verify_peer;
+	bSslVersion = ssl_version;
 	#endif
 
 	#ifdef WITHOUT_SSL
